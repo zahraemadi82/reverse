4a3fe9aae9329f84de57ee2423a6ae8f30325ba6
User <User@DESKTOP-1364VBG>
2024-01-29 17:27:32 +0330

    Add project files.

---
+++ reverse/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter Number : ");
+string number = Console.ReadLine();
+string reverse = "";
+for(int i = 0; i < number.Length; i++)
+{
+    reverse = number[i] + reverse;
+}
+
+Console.Write(reverse);
